6d989df8a0f720ea01b44c831a781fd152ddc37e
sergeytonchenko <randick56@gmail.com>
2020-10-13 23:36:29 +0300

    upd

--- webpack.config.js
+++ webpack.config.js
@@ -89,7 +89,14 @@ module.exports = {
             options: {
               name: '[name].[ext]',
             }
-          }
+          },
+          {
+            test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]'
+            }
+          }, 
         ],
       },
 }
