f3d1891792b5bef74f4abf74541e323b002062d4
sergeytonchenko <randick56@gmail.com>
2020-10-15 11:24:45 +0300

    upd

--- src/js/parralax.js
+++ src/js/parralax.js
@@ -1,7 +1,15 @@
 $(window).scroll(function () {
     const scroll = $(window).scrollTop();
+    const offset = $(".success-wrap").offset().top;
 
+    console.log(offset-200);
     console.log(scroll);
+    console.log((offset-500)/scroll);
+
+    $(".success-penoboard").css({
+        bottom: ((scroll/15)-5) + "%",
+        opacity: (offset-500)/scroll        
+    })
 
     $(".success-balls--1").css({
         bottom: ((scroll/20)-100) + "%"
@@ -13,6 +21,11 @@ $(window).scroll(function () {
 
     $(".success-balls--3").css({
         left: -scroll/60 + "%",
+        top: scroll/40 + "%",
+    })
+
+    $(".success-balls--4").css({
+        right: -scroll/60 + "%",
         top: scroll/40 + "%",
     })
     
