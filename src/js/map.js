1fd457a50f773832905dbd8f0cd3b25d5c6db2dc
sergeytonchenko <randick56@gmail.com>
2020-10-20 21:26:41 +0300

    upd

--- src/js/map.js
+++ src/js/map.js
@@ -1,29 +1,44 @@
 let map;
 
-function initMap() {
+function initMap() {    
 
-    const myLatLng = { lat: 46.667726, lng: 32.689785 };
+        let zoom = 13;
+        let screenWidth = document.documentElement.clientWidth;
+        let center = { lat: 46.668713, lng: 32.632895 };    
+        let markerCenter = { lat: 46.667726, lng: 32.689785 };
+        const root = document.getElementById("map");
 
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 46.668713, lng: 32.632895 },
-        zoom: 13
-    });
+        if (screenWidth < 991) {
+            center = { lat: 46.671726, lng: 32.689785 };
+            zoom = 15;
+            markerCenter = { lat: 46.669726, lng: 32.689785 };
+        }
 
-    new google.maps.Marker({
-        position: myLatLng,
-        map,            
-        icon: {
-            url: './img/marker.png',
-            labelOrigin: new google.maps.Point(30, -14),
-        },                
-        label: {                
-            color: '#b40303',                
-            text: 'Антоновка',
-            fontWeight: 'bold',
-            fontSize: '12px'               
-        },            
-    });
-}
+        console.log(screenWidth);
+        console.log(zoom);
+
+        let options = {
+            center,
+            zoom        
+        }   
+    
+        map = new google.maps.Map(root, options); 
+    
+        new google.maps.Marker({
+            position: markerCenter,
+            map,            
+            icon: {
+                url: './img/marker.png',
+                labelOrigin: new google.maps.Point(30, -14),
+            },                
+            label: {                
+                color: '#b40303',                
+                text: 'Антоновка',
+                fontWeight: 'bold',
+                fontSize: '12px'               
+            },            
+        });    
+};  
 
 initMap();
 
--- src/post.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (!$_POST) exit('No direct script access allowed');
-
-echo '<pre>';
-print_r($_POST);
-echo '</pre>';
-
-# end of file 
