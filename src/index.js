8473f127c1074c497fc45bb39990c01f7d4c38e3
sergeytonchenko <randick56@gmail.com>
2020-10-20 11:16:03 +0300

    upg

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import 'swiper/swiper-bundle.css';
 import '@fancyapps/fancybox/dist/jquery.fancybox.min.css';
 import '@fancyapps/fancybox/dist/jquery.fancybox.min';
+import 'mmenu-js/dist/mmenu.css';
+import 'mmenu-js/dist/mmenu';
+import 'mburger-css/dist/mburger.css';
+import 'mburger-css/dist/mburger';
 import './scss/main.scss';
 import 'jquery-validation';
 import './js/header';
+import './js/mmenu';
 import './js/formLogin';
 import './js/slider';
 import './js/parralax';
---
+++ src/js/mmenu.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+    const $menu = $("#my-menu").mmenu({
+        navbars: [
+            {
+               "position": "top",
+               "content": [
+                  "searchfield"
+               ]               
+            }
+         ],
+         navbar: {
+            "title": "Меню"
+         },
+        extensions: ["theme-white", "position-right", "pagedim-black"],        
+    });
+    const $icon = $("#mmenu-icon");
+    const API = $menu.data( "mmenu" );
+
+    $icon.on( "click", function() {
+        API.open();
+    });
+
+
+    API.bind( "opened", function() {
+       setTimeout(function() {
+          $icon.addClass( "is-active" );
+       }, 100);
+
+       $icon.on( "click", function() {
+          API.close();
+       });
+
+   });
+
+   API.bind( "closed", function() {
+      setTimeout(function() {
+         $icon.removeClass( "is-active" );
+      }, 100);
+
+      $icon.on( "click", function() {
+         API.open();
+      });
+
+   });
+
+});
