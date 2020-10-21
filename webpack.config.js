c0a9a6bf3ae40778b07b0121b692af0d77aba800
sergeytonchenko <randick56@gmail.com>
2020-10-21 08:11:11 +0300

    upd

--- webpack.config.js
+++ webpack.config.js
@@ -104,7 +104,7 @@ module.exports = {
             test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
             loader: 'file-loader',
             options: {
-              name: '[name].[ext]'
+              name: 'fonts/[name].[ext]'
             }
           }           
         ],
