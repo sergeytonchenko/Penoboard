3a530d455097ba1e5657152c8c3d4e8a0539a50e
sergeytonchenko <randick56@gmail.com>
2020-10-21 08:02:36 +0300

    upd

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,8 @@ module.exports = {
         }),
         new CopyWebpackPlugin({
           patterns: [
-            { from: './src/img', to: './img' },            
+            { from: './src/img', to: './img' },
+            { from: './src/fonts', to: './fonts' },            
           ]
         }),
         new webpack.ProvidePlugin({
