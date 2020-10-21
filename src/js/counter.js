742e0b610facf19c69e4d1072e8ec2bf916cb4d2
sergeytonchenko <randick56@gmail.com>
2020-10-21 23:25:25 +0300

    upd

--- src/js/counter.js
+++ src/js/counter.js
@@ -1,38 +1,6 @@
 require ('jq-numscroll/src/js/core')
 
-
-
-$(document).ready(function () {
-
+$(document).ready(function () {  
     $(".numscroller").numScroll();
-
-    // let show1 = true; 
-    // let show2 = true;    
-    // $(window).on("scroll load resize", function () {
-    //     if (!show1) return false; 
-    //     let w_top = $(window).scrollTop(); 
-    //     let e_top = $(".success-territory__num").offset().top; 
-    //     if (w_top + 900 >= e_top) {
-    //         $('.num-count').css('opacity', '1');
-    //         $('.num-count').spincrement({
-    //                 thousandSeparator: "",
-    //                 duration: 1800
-    //             });
-    //         show1 = false;
-    //     }
-    // });
     
-    // $(window).on("scroll load resize", function () {
-    //     if (!show2) return false; 
-    //     let w_top = $(window).scrollTop(); 
-    //     let a_top = $(".success-content__num").offset().top; 
-    //     if (w_top + 900 >= a_top) {
-    //         $('.success-content__num').css('opacity', '1');
-    //         $('.success-content__num').spincrement({
-    //                 thousandSeparator: "",
-    //                 duration: 1800
-    //             });
-    //         show2 = false;
-    //     }
-    // });
 });
