2b4d3baecda26d01f4cd59d7dd3a6825cdde4c5c
sergeytonchenko <randick56@gmail.com>
2020-10-27 23:07:02 +0200

    fix slider scss

--- src/js/slider.js
+++ src/js/slider.js
@@ -3,10 +3,10 @@ import Swiper, { Autoplay, Pagination } from 'swiper';
 Swiper.use([Autoplay, Pagination]);
 
 let swiper = new Swiper('.swiper-container', { 
-    autoplay: {        
-        delay: 3000,
-        disableOnInteraction: false,
-      },
+    // autoplay: {        
+    //     delay: 3000,
+    //     disableOnInteraction: false,
+    //   },
     speed: 1000,
     direction: 'horizontal',
     loop: true, 
